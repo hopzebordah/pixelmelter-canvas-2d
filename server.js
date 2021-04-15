3d7078044882448634c925bdb34cbb499559f0e0
alex peters <aalcocerpeters@gmail.com>
2021-04-15 17:23:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`PixelMelter (2d canvas PoC) running at http://localhost:${PORT}`)
 })
